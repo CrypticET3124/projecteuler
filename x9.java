dd39e377ef7e7e0637ac22f8ec4ccba0da283188
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2021-09-22 17:03:42 -0600

    proj9 formatting

--- x9.java
+++ x9.java
@@ -4,7 +4,7 @@ public class x9 {
         int b = 0;
         int c = 0;
 
-        outer: for (a = 1; a<= 998; a++) {
+        outer: for (a = 1; a <= 998; a++) {
             for (b = 1; b <= 998; b++) {
                 for (c = 1; c <= 998; c++) {
                     if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) && a + b + c == 1000) {
