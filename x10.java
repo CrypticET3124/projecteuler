304340bb06f2bae75b2b6445dded88f002906e2e
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2021-09-22 18:35:34 -0600

    proj10

--- x10.java
+++ x10.java
@@ -1,13 +1,13 @@
 public class x10 {
     public static void main(String[] args) {
-        Long n = 0L;
+        Long n = 2L;
 
-        for (Long x = 1L; x < 2000000; x += 2) {
-            inner: for (Long z = 1L; z <= x; z++) {
+        for (Long x = 3L; x < 2000000L; x += 2L) {
+            inner: for (Long z = 2L; z <= x; z++) {
                 if (z.equals(x)) {
                     n += x;
                     System.out.println(x);
-                } else if (x % z == 0) {
+                } else if (x % z == 0L) {
                     break inner;
                 }
             }
